3b4618e915ff879a1beca34d32aa683e316ff359
maki <jantulamarko@outlook.com>
2022-12-05 20:18:20 +0100

    Add project files.

---
+++ Open-Lab-10.04/Program.cs
@@ -0,0 +1,24 @@
+﻿using Open_Lab_10._03;
+using Open_Lab_10._04;
+using System;
+using System.Security.Cryptography;
+
+namespace Open_Lab_10._04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Book LOTR = new Book();
+            LOTR.Title = "The Lord of the Rings";
+            LOTR.Pages = 9250;
+            LOTR.Category = "epic high-fantasy novel";
+            LOTR.Author = "John Ronald Reuel Tolkien";
+            LOTR.ReleaseDate = 1954;
+            Console.WriteLine(LOTR.ToString());
+
+
+        }
+    }
+}
